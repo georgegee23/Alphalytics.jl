9d964f71dc8e3b93729d38718107d4d864e29496
Documenter.jl <documenter@juliadocs.github.io>
2025-03-27 19:20:53 +0000

    build based on 3ae32b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Alphalytics","category":"page"},{"location":"#Alphalytics","page":"Home","title":"Alphalytics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Alphalytics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Alphalytics]","category":"page"}]
+}
